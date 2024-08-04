9d6cc8be6dda815acbf015ff570be518f728e190
Jonyrsc <161210606+Jonyrsc@users.noreply.github.com>
2024-08-03 20:53:52 -0500

    jony.cpp
    Paso 00
    
    Nivel: Facil

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c++/jony.cpp
@@ -0,0 +1,28 @@
+//https://visualstudio.microsoft.com/es/vs/features/cplusplus/
+/*
+Este es mi ´primer programa en C++ del 2024
+y estoy dispouesto aprender nuevos retos
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int variable;				
+int constant = 27;
+
+int variable_entera;
+float variable_decimal;
+char caracter_no_numerico[27];
+string cadena_de_caracteres;
+bool f = true;
+
+int main(){
+cout <<"Hola, C++!";
+
+cout <<"Hola, int";
+cout << "Hola,float";
+cout << "Hola char";
+cout << "Hola string";
+cout << "Hola bool";
+};
